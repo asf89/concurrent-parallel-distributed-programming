0a3cb369f6b7353bf50dcba73ca7b9e74f9107ac
asf89 <asf@cesar.school>
2020-05-31 18:50:14 -0400

    added dispatch server

---
+++ exam_distributed_programming/dispatchserver/src/main/java/school/cesar/sd/dispatch/dispatchserver/DispatchserverApplication.java
@@ -0,0 +1,13 @@
+package school.cesar.sd.dispatch.dispatchserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DispatchserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DispatchserverApplication.class, args);
+	}
+
+}
