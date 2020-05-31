6acddcc35fb74161cfce06d337fada965f13ee88
asf89 <asf@cesar.school>
2020-05-31 18:44:14 -0400

    added cart client

---
+++ exam_distributed_programming/cartClient/src/main/java/school/cesar/sd/cart/cartClient/CartClientApplication.java
@@ -0,0 +1,13 @@
+package school.cesar.sd.cart.cartClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartClientApplication.class, args);
+	}
+
+}
