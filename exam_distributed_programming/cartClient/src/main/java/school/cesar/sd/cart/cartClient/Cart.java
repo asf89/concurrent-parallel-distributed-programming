357fdc4b55a2c6045328be5986a9711c3ae0d1cc
asf89 <asf@cesar.school>
2020-05-31 23:05:57 -0400

    Overrided run() function

--- exam_distributed_programming/cartClient/src/main/java/school/cesar/sd/cart/cartClient/Cart.java
+++ exam_distributed_programming/cartClient/src/main/java/school/cesar/sd/cart/cartClient/Cart.java
@@ -12,7 +12,9 @@ public class Cart implements CommandLineRunner {
 
     @Autowired
     RabbitTemplate template;
-    public void run(String[] args) throws Exception {
+
+    @Override
+    public void run(String... args) throws Exception {
 
         ArrayList<String> skus = new ArrayList<String>();
         skus.add("kjp12");
@@ -20,6 +22,12 @@ public class Cart implements CommandLineRunner {
         skus.add("crz8s");
 
         Customer customer = new Customer("asf@email.com", "80471-962", skus, 671.80); 
+
+        System.out.println("Sending cart data...");
+
+        template.convertAndSend("Shopping Cart", customer);
+
+        System.out.println("Data sent.");
 
     }
     
