c7f5d7dcbfc76a079f09451f9fd7155bb5682b9b
asf89 <asf@cesar.school>
2020-05-31 20:49:00 -0400

    added customer information

--- exam_distributed_programming/cartClient/src/main/java/school/cesar/sd/cart/cartClient/Cart.java
+++ exam_distributed_programming/cartClient/src/main/java/school/cesar/sd/cart/cartClient/Cart.java
@@ -1,5 +1,7 @@
 package school.cesar.sd.cart.cartClient;
 
+import java.util.ArrayList;
+
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -11,6 +13,13 @@ public class Cart implements CommandLineRunner {
     @Autowired
     RabbitTemplate template;
     public void run(String[] args) throws Exception {
+
+        ArrayList<String> skus = new ArrayList<String>();
+        skus.add("kjp12");
+        skus.add("mvn48");
+        skus.add("crz8s");
+
+        Customer customer = new Customer("asf@email.com", "80471-962", skus, 671.80); 
 
     }
     
