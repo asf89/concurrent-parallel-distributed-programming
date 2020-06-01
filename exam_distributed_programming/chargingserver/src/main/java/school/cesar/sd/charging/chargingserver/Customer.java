d689b751813db85661ec9be76d327533a8f6ac1f
asf89 <asf@cesar.school>
2020-05-31 23:37:46 -0400

    added Customer class

---
+++ exam_distributed_programming/chargingserver/src/main/java/school/cesar/sd/charging/chargingserver/Customer.java
@@ -0,0 +1,27 @@
+package school.cesar.sd.charging.chargingserver;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Customer implements Serializable {
+
+    private String email;
+    private String cep;
+    private ArrayList<String> skus;
+    private double cost;
+
+    public Customer(@JsonProperty("e-mail") String email
+                  , @JsonProperty("CEP") String cep
+                  , @JsonProperty("skus") ArrayList<String> skus
+                  , @JsonProperty("cost") double cost) {
+        
+        this.email = email;
+        this.cep = cep;
+        this.skus = skus;
+        this.cost = cost;
+
+    }
+    
+}
